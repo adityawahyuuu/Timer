5874295477a1a9305531acc3ad25bdb49f447cf1
adityawahyuuu_ <75268457+adityawahyuuu@users.noreply.github.com>
2021-07-20 04:30:46 +0700

    Add files via upload

---
+++ timer/sketch_jul13a/sketch_jul13a.ino
@@ -0,0 +1,40 @@
+/*
+ * Aditya Wahyu Pradhana
+ * Timer
+ * June 17, 2021
+*/
+
+#define over 60000
+
+void setup()
+{
+  Serial.begin(9600);
+  TCCR1A = 0;
+  TCCR1B = 0;
+  TCNT1 = 3035;
+  TCCR1B |= (1 << CS12) | (1 << CS10);
+  TIMSK |= (1 << TOIE1);
+  DDRD |= (1 << PD2);
+}
+
+ISR(TIMER1_OVF_vect)
+{
+  TCNT1 = 3035;
+
+  if (millis() >= over)
+  {
+    Serial.println("time over");
+    PORTD &= ~(1 << PD2);
+    
+  }
+  else if (millis() < over)
+  {
+    Serial.println("ON!!");
+    PORTD |= (1 << PD2);
+  }
+}
+
+void loop()
+{
+  
+}
